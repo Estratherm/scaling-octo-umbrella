07907f41a694376e6dbba4ab3bc2e5b60a3d790f
SiDoK <85799853+Estratherm@users.noreply.github.com>
2021-06-12 22:16:27 +0300

    test
    test

---
+++ js/adaptiv.js
@@ -0,0 +1,65 @@
+"use strict"
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.burger__arrow');
+	let menuArrows2 = document.querySelectorAll('.buy__item--burger');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	} 
+
+	if (menuArrows2.length > 0) {
+		for (let index = 0; index < menuArrows2.length; index++) {
+			const menuArrow2 = menuArrows2[index];
+			menuArrow2.addEventListener("click", function (e) {
+				menuArrow2.parentElement.classList.toggle('_active');
+			});
+	}
+}
+
+
+} else {
+	document.body.classList.add('_pc');
+
+	let menuArrows = document.querySelectorAll('.burger__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+}
